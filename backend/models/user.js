fc94c3fb942e64634f3afd58ee953377e2d777cc
Shivam Gupta <sgpt2006@gmail.com>
2026-01-01 12:18:53 +0530

    Add missing User model

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,13 +1,23 @@
 const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema(
+const userSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
   { timestamps: true }
 );
 
-module.exports = mongoose.model("User", UserSchema);
+module.exports = mongoose.model("User", userSchema);
 
